be19e4c743c65777eebd59c6ee0c230ea25af459
fengxiongfei <2512984312@qq.com>
2024-05-08 20:19:33 +0800

    3

--- wxappDjango/settings.py
+++ wxappDjango/settings.py
@@ -34,7 +34,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "lessons.apps.LessonsConfig"
+    #"lessons.apps.LessonsConfig",
+    "lessons"
 ]
 
 MIDDLEWARE = [
